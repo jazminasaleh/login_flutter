0ef80fbf2320eaa1055585dcd28b06d6d593c69e
jazmin saleh peña <jazminasaleh@gmail.com>
2022-08-18 09:42:15 -0500

    Primer commit

---
+++ android/app/src/main/kotlin/com/example/loguin_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.loguin_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/screens.dart
@@ -0,0 +1,3 @@
+export 'package:loguin_flutter/screens/home_screen.dart';
+export 'package:loguin_flutter/screens/login_screen.dart';
+
